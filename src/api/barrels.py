cef50c6f2596a0f5596766c81576d85a06bd78e9
joseespinoza15432 <jose.espinoza15432@gmail.com>
2024-10-03 09:03:43 +0000

    barrelchanges

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -23,9 +23,10 @@ class Barrel(BaseModel):
 
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
-    
+
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = num_green_potions + 1"))
+        result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold - :price, num_green_ml = numgreenml + :ml_per_barrel"))
+    connection.execute(result, {"price": Barrel.price, "ml_per_barrel": Barrel.ml_per_barrel})
 
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
@@ -38,23 +39,22 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     print(wholesale_catalog)
 
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_green_potions, gold FROM global_inventory")).fetchone()
-
+        result = connection.execute(sqlalchemy.text("SELECT num_green_potions, gold FROM global_inventory"))
     
-    if 'num_green_potion' in result < 10 and 'gold' in result > Barrel.price and Barrel.potion_type == [0,100,0,0]:
+    row = result.fetchone()
+
+    numberofpotions = row['num_green_potion']
+    amountofgold = row['gold']
+
+    greenpotion = wholesale_catalog[0]
+
+
+    if numberofpotions < 10 and amountofgold > greenpotion.price and greenpotion.potion_type == [0, 100, 0, 0]:
+        maxamount = amountofgold // greenpotion.price
         return [
             {
                 "sku": "SMALL_GREEN_BARREL",
-                "quantity": 1,
+                "quantity": maxamount,
             }
         ]
     
-
-    """row = result.fetchone()
-    if row and row['num_green_potions'] < 10:
-        return [
-            {
-                "sku": "SMALL_GREEN_BARREL",
-                "quantity": 1,
-            }
-        ]"""
