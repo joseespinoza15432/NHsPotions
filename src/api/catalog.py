b2b5bbe05b3bcdb96e8516750698cf944fbb6213
joseespinoza15432 <jose.espinoza15432@gmail.com>
2024-10-20 22:14:18 -0700

    v3 catalog changes

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -16,36 +16,24 @@ def get_catalog():
 
     with db.engine.begin() as connection:
 
-        result = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory")).fetchone()
+        result = connection.execute(sqlalchemy.text("SELECT potion_id, price, potion_type_red, potion_type_green, potion_type_blue, potion_type_dark, potion_quantity FROM custom_potions")).fetchall()
+
+        for potion in result:
+            red_ml = potion.num_red_ml
+            green_ml = potion.num_green_ml
+            blue_ml = potion.num_blue_ml
+            dark_ml = potion.num_dark_ml
+            cost = potion.price
+            quantity = potion.potion_quantity
+
+            inventory_catalog.append(
+            {
+                "sku": f"NHsCustomPotion_{potion.potion_id}",
+                "name": "Custom Potion",
+                "quantity": quantity,
+                "price": cost,
+                "potion_type": [red_ml, green_ml, blue_ml, dark_ml]
+            }
+            )
         
-        if result.num_red_potions > 0:
-            inventory_catalog.append(
-                {
-                    "sku": "red_POTION_0",
-                    "name": "green potion",
-                    "quantity": result.num_red_potions,
-                    "price": 50,
-                    "potion_type": [100, 0, 0, 0],
-                }
-            )
-        if result.num_green_potions > 0:
-            inventory_catalog.append(
-                {
-                    "sku": "green_POTION_0",
-                    "name": "green potion",
-                    "quantity": result.num_green_potions,
-                    "price": 50,
-                    "potion_type": [0, 100, 0, 0],
-                }
-            )
-        if result.num_blue_potions > 0:
-            inventory_catalog.append(
-                {
-                    "sku": "blue_POTION_0",
-                    "name": "green potion",
-                    "quantity": result.num_blue_potions,
-                    "price": 50,
-                    "potion_type": [0, 0, 100, 0],
-                }
-            )
     return inventory_catalog
